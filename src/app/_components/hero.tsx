2a7a183814842e9b91dfb92c482ece2077ee48a6
matheus claubert <mattheus_claubert@hotmail.com>
2025-05-23 21:55:39 -0300

    feat: responsividade da hero

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,11 +1,11 @@
-import {    } from @phosphor-icons/react"
+import { WhatsappLogoIcon } from "@phosphor-icons/react/dist/ssr";
 
 export function Hero() {
     return (
         <section className="bg-violet-950 text-white relative overflow-hidden">
-            <div>
-                <article>
-                    <div>
+            <div className="container mx-auto px-4 py-16 lg:py-24">
+                <article className="grid grid-cols-1 md:grid-cols-2 gap-8 relative">
+                    <div className="space-y-6">
                         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-10">Seu pet merece cuidado, carinho e atenção especial
                         </h1>
                         <p className="lg:text-lg">Oferecemos os melhores serviços para garantir o bem-estar e a felicidade do seu amigo de 4 patas
@@ -13,8 +13,10 @@ export function Hero() {
 
 
                         <a href="#"
-                            className="bg-green-500 px-5 py-2 rounded-md font-semibold flex items-center justify-center w-fit"
-                        >Contato via Whatsapp
+                            className="bg-green-500 px-5 py-2 rounded-md font-semibold flex items-center justify-center w-fit gap-2"
+                        >
+                            <WhatsappLogoIcon className="w-5 h-5 " />
+                            Contato via Whatsapp
                         </a>
 
                         <div className="mt-8">
@@ -23,6 +25,9 @@ export function Hero() {
                             </p>
                         </div>
 
+                    </div>
+                    <div>
+                        <h1>FOTO DO CACHORRO</h1>
                     </div>
                 </article>
             </div>
